1f52d7778430709fe03aa4900b6befddfe32202f
tsunodajapa <gabrieltsunoda@gmail.com>
2022-08-17 18:10:39 -0300

    fix: teste

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,21 +1,13 @@
-import { useEffect, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Cadastro from "../pages/Cadastro";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
 
 const RoutesMain = () => {
-  const [login, setLogin] = useState(false);
-
-  useEffect(() => {
-    const token = JSON.parse(localStorage.getItem("hub:token"));
-
-    token && setLogin(true);
-  });
-
+  console.log('teste');
   return (
     <Routes>
-      <Route path="/" element={<Login login={login} setLogin={setLogin} />} />
+      <Route path="/" element={<Login />} />
 
       <Route path="/cadastro" element={<Cadastro />} />
 
